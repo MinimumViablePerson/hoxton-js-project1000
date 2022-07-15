e165de2701faffa20ec9ba4d4e0526402d2a4668
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-07-15 15:24:28 +0100

    new message

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 function render () {
   const mainEl = document.querySelector('#app')
   const divEl = document.createElement('div')
-  divEl.textContent = 'text in a div, hug me im lonely!'
+  divEl.textContent = 'text in a div, hug me im lonely!!!!!'
   mainEl.append(divEl)
 }
 render()

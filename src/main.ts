fe564116e377477414c1702df3c83b1cabd89fd7
Ed 'Catboy' Putans <edputans@gmail.com>
2022-07-15 15:12:17 +0100

    added  some stuff so nico can feel sum code

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import './style.css'
 
-function render () {}
+function render() {
+  const mainEl = document.querySelector('#app')
+  const divEl = document.createElement('div');
+  divEl.textContent = 'text in a div, eat me!'
+  mainEl.append(divEl);
+}
+render();
